1e224566c3486d1d2bfa404fb26bfe938ba53b86
Saharat Paynok <saharat.paynok@gmail.com>
2022-09-21 21:12:14 +0700

    initial commit

---
+++ 2.values.go
@@ -0,0 +1,19 @@
+package main
+
+/*
+	Go has various value type including strings, integers, floats, booleans etc
+*/
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
