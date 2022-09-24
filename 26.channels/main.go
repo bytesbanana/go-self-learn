0bd84c056c6651348be584383ffab71b59e224a4
Saharat Paynok <saharat.paynok@gmail.com>
2022-09-25 00:55:11 +0700

    adding channels example

--- 26.channels/main.go
+++ 26.channels/main.go
@@ -5,6 +5,13 @@ import (
 	"time"
 )
 
+/*
+
+* Channels are the pipes that connect concurrent goroutines.
+* You can send values into channels from one goroutine and receive those values into another goroutine.
+
+ */
+
 func main() {
 
 	/*
