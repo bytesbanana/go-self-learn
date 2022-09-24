bdeb149c04c99289e5141b43947906f4b02d1fac
Saharat Paynok <saharat.paynok@gmail.com>
2022-09-25 00:53:05 +0700

    adding channels example

---
+++ 26.channels/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	/*
+	* Create a new channel with make(chan val-type).
+	* Channels are typed by the values they convey.
+	 */
+	fmt.Println("Start:", time.Now())
+	messages := make(chan string)
+
+	go func() {
+		//* Send a value into a channel using the channel <- syntax
+		time.Sleep(time.Second * 2)
+		fmt.Println("Excute:", time.Now())
+		messages <- "ping"
+	}()
+
+	fmt.Println("Recieved:", time.Now())
+	msg := <-messages
+	// fmt.Println(<-messages)
+	fmt.Println("Done:", time.Now())
+	fmt.Println(msg)
+	// !By default sends and receives block until both the sender and receiver are ready
+	// !This property allowed us to "wait at the end of our program" for the "ping" message without having to use any other synchronizatio
+
+}
