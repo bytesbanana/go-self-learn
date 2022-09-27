ed4eefeb6f22b23234e9b6cc9547d185a5b1c81a
Saharat Paynok <saharat.paynok@gmail.com>
2022-09-27 20:59:54 +0700

    adding timeout example

---
+++ 31.timeout/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c1 <- "result 1"
+	}()
+
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(1 * time.Second): //time.After return "chan time.Time"
+		fmt.Println("timeout 1") // it will be print timeout because timeout faster thant line 12
+	}
+
+	c2 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "result 2"
+	}()
+
+	select {
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(3 * time.Second):
+		fmt.Println("timeout 2")
+	}
+}
