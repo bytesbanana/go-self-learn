cb8aa25df583d361caf4585e910d8fe2b30d5395
Saharat Paynok <saharat.paynok@gmail.com>
2022-10-07 22:47:22 +0700

    adding recover example

---
+++ 47.recover/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func mayPanic() {
+	panic("a problem")
+}
+
+func main() {
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recover from error:", r)
+		}
+	}()
+
+	mayPanic()
+
+	fmt.Println("After mayPanic()")
+
+}
